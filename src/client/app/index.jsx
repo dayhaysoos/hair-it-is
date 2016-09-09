61870ba6e7b810419a284dca4b80072ddad59472
Nick DeJesus <ndejesus@sapient.com>
2016-09-09 01:02:26 -0400

    adding more things

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {render} from 'react-dom';
+import Header from './components/Header/Header.jsx';
 
 class App extends React.Component {
   render () {
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,108 @@
-var webpack = require('webpack');
-var path = require('path');
+const path = require('path');
+const merge = require('webpack-merge');
+const webpack = require('webpack');
+const NpmInstallPlugin = require('npm-install-webpack-plugin');
 
-var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
-var APP_DIR = path.resolve(__dirname, 'src/client/app');
+const TARGET = process.env.npm_lifecycle_event;
+const PATHS = {
+  app: path.join(__dirname, 'src/client/app'),
+  build: path.join(__dirname, 'src/client/build')
+};
 
-var config = {
-  entry: APP_DIR + '/index.jsx',
+console.log(TARGET);
+
+process.env.BABEL_ENV = TARGET;
+
+const config = {
+  entry: {
+    app: PATHS.app + '/index'
+  },
+  resolve: { 
+    extensions: ['', '.js', '.jsx']
+  },
   output: {
-    path: BUILD_DIR,
+    path: PATHS.build,
     filename: 'bundle.js'
   },
-  
-    module : {
-    loaders : [
+  module: {
+    loaders: [
       {
-        test : /\.jsx?/,
-        include : APP_DIR,
-        loader : 'babel'
+        test: /\.css$/,
+        //loader: ExtractTextPlugin.extract('style-loader', 'css-loader'),
+        loaders: ['style', 'css', 'sass'],
+        include: PATHS.app
+      },
+      {
+        test: /\.scss$/,
+        //loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader')
+        loaders: ['style', 'css', 'sass']
+      },
+      {
+        test: /\.jsx?$/,
+        loaders: ['babel?cacheDirectory'],
+        include: PATHS.app
+      },
+      {
+        test: /\.json?$/,
+        loaders: ['json'],
+        include: PATHS.app
       }
     ]
   }
-};
+}
+
+// var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
+// var APP_DIR = path.resolve(__dirname, 'src/client/app');
+
+// var config = {
+//   entry: APP_DIR + '/index.jsx',
+//   output: {
+//     path: BUILD_DIR,
+//     filename: 'bundle.js'
+//   },
+  
+//     module : {
+//     loaders : [
+//       {
+//         test : /\.jsx?/,
+//         include : APP_DIR,
+//         loader : 'babel'
+//       }
+//     ]
+//   }
+// };
 
 
-module.exports = config;
+//module.exports = config;
+
+if(TARGET ==='start' || !TARGET) {
+  module.exports = merge(config, { 
+    devtool: 'eval-source-map',
+    devServer: {
+      contentBase: PATHS.build,
+
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true,
+
+      //display only errors to reduce the amount of output
+      stats: 'errors-only',
+
+      //parse host and port from env so this is 
+      //easy to customize
+      host: process.env.HOST,
+      port: process.env.port
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new NpmInstallPlugin ({
+        save: true //--save
+      })
+    ]
+  });
+}
+
+if(TARGET === 'build') {
+  module.exports = merge(config, {});
+}
