65f7ab67dc2bd2a43638a2691061697f1227f265
Nick DeJesus <ndejesus@sapient.com>
2016-09-12 12:10:58 -0400

    added About page

---
+++ src/client/app/components/About/About.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class About extends React.component {
+	render () {
+		return <h1 className="about">About Page</h1>
+	}
+}
+
+export default About;
--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {render} from 'react-dom';
-import {Router, Route, hashHistory, browserHistor } from 'react-router';
+import {Router, Route, hashHistory, browserHistory } from 'react-router';
 import Home from './components/Home/Home.jsx';
 
 //import CSS
