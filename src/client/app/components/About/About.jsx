969e0f71ca76890f61d50a31fefaeef78ccfb7aa
Nick DeJesus <ndejesus@sapient.com>
2016-09-12 16:42:18 -0400

    trying to get browser history to work

--- src/client/app/components/About/About.jsx
+++ src/client/app/components/About/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-class About extends React.component {
+class About extends React.Component {
 	render () {
 		return <h1 className="about">About Page</h1>
 	}
--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import {render} from 'react-dom';
 import {Router, Route, hashHistory, browserHistory } from 'react-router';
+import createBrowserHistory from 'history';
 import Home from './components/Home/Home.jsx';
+import About from './components/About/About.jsx';
 
 //import CSS
 import CSS from '../assets/styles/main.scss';
 
-
  const router = (
-  <Router history={hashHistory}>
+  <Router history={browserHistory}>
     <Route path="/" component={Home} />
+    <Route path="/about" component={About} />
   </Router>
 )
 
